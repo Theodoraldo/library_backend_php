0750f2e223faac2037d272daffdb4ad2375da54a
Theodoraldo <theodoraldo@gmail.com>
2024-03-28 17:20:55 +0000

    Setting genres model and table

--- database/migrations/2024_03_28_141901_create_genres_table.php
+++ database/migrations/2024_03_28_141901_create_genres_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
+            $table->string('genre_name');
+            $table->string('description');
             $table->timestamps();
         });
     }
