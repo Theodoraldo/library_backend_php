27ec848c75b66c7261a4a5d3bf6a1678fadc8c41
Theodoraldo <theodoraldo@gmail.com>
2024-03-28 17:20:13 +0000

    Setting books model and table

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'author_id',
+        'genre_id',
+        'notes',
+        'published_date',
+        'available_copies',
+        'cover_image',
+        'pages',
+    ];
 }
--- database/migrations/2024_03_28_142020_create_books_table.php
+++ database/migrations/2024_03_28_142020_create_books_table.php
@@ -13,7 +13,18 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->date('published_date');
+            $table->integer('available_copies');
+            $table->string('cover_image')->nullable();
+            $table->integer('pages');
+            $table->string('notes')->nullable();
+            $table->unsignedBigInteger('author_id')->constrained();
+            $table->unsignedBigInteger('genre_id')->constrained();
             $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('authors');
+            $table->foreign('genre_id')->references('id')->on('genres');
         });
     }
 
