ce05421274767ebe9821bd426c175c5d4c420b88
Theodoraldo <theodoraldo@gmail.com>
2024-03-28 17:21:16 +0000

    Setting library patrons model and table

--- app/Models/LibraryPatron.php
+++ app/Models/LibraryPatron.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class LibraryPatron extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'firstname',
+        'lastname',
+        'email',
+        'contact',
+        'address',
+        'city',
+        'state',
+        'location',
+        'identity_card',
+        'identity_no',
+    ];
 }
--- database/migrations/2024_03_28_142009_create_library_patrons_table.php
+++ database/migrations/2024_03_28_142009_create_library_patrons_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('library_patrons', function (Blueprint $table) {
             $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('contact')->unique();
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('location');
+            $table->string('identity_card')->unique();
+            $table->string('identity_no')->unique();
+            $table->boolean('engagement')->default(false);
             $table->timestamps();
         });
     }
