f9183d5201add931f6c8399a50b601a2ab97478f
Theodore Osei Asimeng <theodoraldo@gmail.com>
2024-05-09 10:42:04 +0000

    Add patron records to attendance resources

--- app/Http/Resources/V1/AttendanceResource.php
+++ app/Http/Resources/V1/AttendanceResource.php
@@ -4,21 +4,18 @@ namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\V1\LibraryPatronResource;
+
 
 class AttendanceResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
             'checkIn' => $this->check_in,
             'checkOut' => $this->check_out,
-            'patronId' => $this->library_patron_id,
+            'patron' => optional($this->library_patron)->exists ? new LibraryPatronResource($this->library_patron) : null,
         ];
     }
 }
