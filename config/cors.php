05b765d377a8d6bd32ff01867b38650580066ec2
Theodoraldo <theodoraldo@gmail.com>
2026-03-30 02:08:16 +0000

    Modify CORS and composer files

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'signup', 'signin', 'signout', 'user'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
